edfcac3b667eddd1da7d374a8925947ac469eec1
soumen-404 <145795961+soumen-404@users.noreply.github.com>
2023-09-25 12:53:56 +0530

    Add files via upload

---
+++ Landing Page-soumen/app.js
@@ -0,0 +1,10 @@
+var sidemen = document.getElementById("sidemenu")
+
+function openmenu() {
+    sidemen.style.right = "-54px";
+
+}
+function closemenu() {
+    sidemen.style.right = "-200px";
+
+}
